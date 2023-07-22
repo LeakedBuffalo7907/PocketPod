876488d0ee48fd0a94c4b93e747f6685a23d6180
LeakedBuffalo7907 <69818555+LeakedBuffalo7907@users.noreply.github.com>
2023-07-21 20:05:10 -0400

    s

--- install.lua
+++ install.lua
@@ -3,10 +3,10 @@ local apiURL = "http://api.github.com/repos/LeakedBuffalo7907/PocketPod/releases
 local baseRepoURL = "https://raw.githubusercontent.com/LeakedBuffalo7907/PocketPod/main"
 local args = {...}
 local skipcheck = false
+local currentVersion = "0.01"
 if args and args[1] == "y" then
   skipcheck = true
 end
-
 
 http.request(apiURL)
 print("Made request to " .. apiURL)
@@ -28,9 +28,11 @@ local updated = false
     shell.run("wget " .. baseRepoURL .. "/lib/youcubeapi.lua /lib/youcubeapi.lua")
     shell.run("wget " .. baseRepoURL .. "/lib/basalt.lua /lib/basalt.lua")
     if updated then
-      print("Pocket Pod has Updated")
+      settings.load()
+      print("Pocket Pod has Updated " .. currentVersion)
+
     else 
-      print("Pocket Pod Installed")
+      print("Pocket Pod Installed " .. currentVersion)
     end
     return
     
