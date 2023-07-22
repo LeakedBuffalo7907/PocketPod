5f8da7e69cdde209f1b75faca31e641f918eb668
LeakedBuffalo7907 <69818555+LeakedBuffalo7907@users.noreply.github.com>
2023-07-22 00:37:19 -0400

    s

--- install.lua
+++ install.lua
@@ -3,13 +3,16 @@ local apiURL = "http://api.github.com/repos/LeakedBuffalo7907/PocketPod/releases
 local baseRepoURL = "https://raw.githubusercontent.com/LeakedBuffalo7907/PocketPod/main"
 local args = {...}
 local skipcheck = false
-local currentVersion = "0.0161"
+local currentVersion = "0.017"
 if args and args[1] == "y" then
   skipcheck = true
 end
 
-http.request(apiURL)
-print("Made request to " .. apiURL)
+local function updateFile(name, path)
+  fs.delete(path .. name)
+  shell.run("wget " .. baseRepoURL .. path .. name .. " " .. path .. name)
+end
+
 local updated = false
   if fs.exists("/PodOS.lua") then
     if fs.exists("/CurrentVersion.txt") then
@@ -23,26 +26,29 @@ local updated = false
     end
     updated = true
     print("Pocket Pod Already Exists, Deleting old install")
-    fs.delete("/CurrentVersion.txt")
-    fs.delete("/PodOS.lua")
-    fs.delete("/lib/semver.lua")
-    fs.delete("/lib/youcubeapi.lua")
-    fs.delete("/lib/basalt.lua")
-  end
+    updateFile("/", "CurrentVersion.txt")
+    updateFile("/", "PodOS.lua")
+    updateFile("/lib/", "semver.lua")
+    updateFile("/lib/", "youcubeapi.lua")
+    updateFile("/lib/", "basalt.lua")
+    print("Pocket Pod has Updated " .. currentVersion)
+    return
 
+  else
     print("Installing now")
     shell.run("wget " .. baseRepoURL .. "/PodOS.lua /PodOS.lua")
-
-    print("Downloading libraries right now")
     shell.run("wget " .. baseRepoURL .. "/lib/semver.lua /lib/semver.lua")
     shell.run("wget " .. baseRepoURL .. "/lib/youcubeapi.lua /lib/youcubeapi.lua")
     shell.run("wget " .. baseRepoURL .. "/lib/basalt.lua /lib/basalt.lua")
     shell.run("wget " .. baseRepoURL .. "/CurrentVersion.txt /CurrentVersion.txt")
+    print("Pocket Pod Installed " .. currentVersion)
+
+  end
     if updated then
-      print("Pocket Pod has Updated " .. currentVersion)
+      
 
     else 
-      print("Pocket Pod Installed " .. currentVersion)
+      
     end
 
     return
