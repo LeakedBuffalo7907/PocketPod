f1b0abb2fcb8d1e568fac77aed7a9be7b670a1b2
ezfo <69818555+LeakedBuffalo7907@users.noreply.github.com>
2023-08-03 00:08:22 -0400

    sss

--- PodOS.lua
+++ PodOS.lua
@@ -3,124 +3,39 @@ if periphemu then -- probably on CraftOS-PC
     config.set("standardsMode",true)
 end
 
-local dfpwm = require("cc.audio.dfpwm")
-local version = "2.5.0"
+local version = "1.0.0"
 local args = {...}
 local pod = {}
 local speaker = peripheral.find("speaker")
-local v = require("/lib/semver")
-local SongsList = {}
-local YouCubeAPI = require("/lib/youcubeapi")
+local GlobalSongsList = {}
+local speakerlib = require("/lib/speakerlib")
 local webserver_URL = "https://computercraftmp3.leakedbuffalo79.repl.co"
 
 if not speaker then -- Check if there is a speaker
   error("No Speaker",0)
 end
+
 local function getSongsList() 
   local SongsFile, msg = http.get(webserver_URL .. "/songs")
   if not SongsFile then
     error(msg)
   end
 
-  SongsList = textutils.unserialiseJSON(SongsFile.readAll())
+  GlobalSongsList = textutils.unserialiseJSON(SongsFile.readAll())
   SongsFile.close()
   
-  if not SongsList then
+  if not GlobalSongsList then
       error("json data malformed",0)
   end
 
 end
-local booted = false;
-local function drawEntries()
-  local w, h = term.getSize()
-    term.clear()
-    term.setCursorPos((w - #"PodOS") / 2, 2)
-    term.setTextColor(16384)
-    term.write("PodOS")
-    --for i = 1, SongsList do
-
-   -- end
-    term.setCursorPos(5, h - 3)
-    term.write("test line 1")
-    term.setCursorPos(5, h - 2)
-    term.write("test line 2")
-    term.setCursorPos(5, h - 1)
-    term.write("test line 3")
-end
-
-local selection = 1
-while booted do
-  local event, key = os.pullEvent("key")
-  if key or event then 
-    drawEntries();
-  end
-  if key == keys.up and selection > 1 then
-    selection = selection - 1
-    drawEntries(selection, entries)
-  elseif key == keys.down and selection < 3 then
-    selection = selection + 1
-    drawEntries(selection, entries)
-  elseif key == keys.enter then
-    if selection == 1 then 
-      -- button 1
-    end
-  end
-end
-
-pod.bootup = function ()
-  drawEntries()
-  getSongsList()
-  booted = true;
-end
 
 pod.play = function (arguments)
-    if not arguments or not arguments[1] then
-      print("No Song Provided")
-      return
-    end
-    local url = arguments[1]
-
-    local youcubeapi = YouCubeAPI.API.new()
-    local audiodevice = YouCubeAPI.Speaker.new(speaker)
-
-    audiodevice:validate()
-    youcubeapi:detect_bestest_server()
     
-    local function run(_url, no_close)
-
-      local chunkindex = 0
-
-      while true do
-        local chunk = youcubeapi:get_chunk(chunkindex, _url)
-          if chunk == "done" then
-            if data.playlist_videos then
-                return data.playlist_videos
-            end
-
-            if no_close then
-                return
-            end
-
-            youcubeapi.websocket.close()
-            return
-          end
-          audiodevice:write(chunk)
-          chunkindex = chunkindex + 1
-      end
-    end
-
-    local playlist_videos = run(url)
-
-    if playlist_videos then
-        for i, id in pairs(playlist_videos) do
-            run(id, true)
-        end
-    end
 end
 
 
 command = table.remove(args, 1)
-pod.index = index
 
 if pod[command] then
     pod[command](args)
--- install.lua
+++ install.lua
@@ -1,61 +1,44 @@
 local baseRepoURL = "http://raw.githubusercontent.com/LeakedBuffalo7907/PocketPod/main"
-local currentVersion = "0.027"
 
-local function updateFile(path, name)
-  fs.delete(path .. name)
-  local newFile = http.get(baseRepoURL .. path .. name)
-  local F = fs.open(path .. name, "w")
-  F.write(newFile.readAll())
-  newFile.close()
-  F.close()
-  print(name .. " Updated")
-end
+
 local function downloadFile(path, name)
+  if fs.exists(path .. name) then
+    fs.delete(path .. name)
   local F = fs.open(path .. name, "w")
   F.write(http.get(baseRepoURL .. path .. name).readAll())
   F.close()
   print(name .. " Downloaded")
 end
 
-local updated = false
-  if fs.exists("/PodOS.lua") then
-    if fs.exists("/CurrentVersion.txt") then
-      local h = fs.open("/CurrentVersion.txt", "r")
-      if string.find(h.readAll(), currentVersion) then 
-        print("Pocket Pod Up To Date " .. currentVersion)
-        h.close()
-        return 
-      end
-      h.close()
-    end
-    updated = true
-    print("Pocket Pod Already Exists, Deleting old install")
-    updateFile("/", "CurrentVersion.txt")
-    updateFile("/", "PodOS.lua")
-    updateFile("/lib/", "semver.lua")
-    updateFile("/lib/", "youcubeapi.lua")
-    updateFile("/lib/", "basalt.lua")
-    print("Pocket Pod has Updated " .. currentVersion)
-    return
+  local uptodate = false
+  local currentVersion = "0.00"
+  local oldUser = fs.exists("/CurrentVersion.txt")
+  if oldUser then
+    local h = fs.open("/CurrentVersion.txt", "r")
+    local webversion = http.get(baseRepoURL .. "/CurrentVersion.txt")
+    currentVersion = webversion.readAll()
+    uptodate = string.find(h.readAll(), currentVersion)
+    h.close()
+    webversion.close()
+  end
 
+  if uptodate then
+    print("Pocket Pod Up To Date " .. currentVersion)
+    return
+  end
+
+  if oldUser then
+    print("Detected old install, Updating now")
   else
     print("Installing now")
-    downloadFile("/", "CurrentVersion.txt")
-    downloadFile("/", "PodOS.lua")
-    downloadFile("/lib/", "semver.lua")
-    downloadFile("/lib/", "youcubeapi.lua")
-    downloadFile("/lib/", "basalt.lua")
-    print("Pocket Pod Installed " .. currentVersion)
-
   end
-    if updated then
-      
-
-    else 
-      
-    end
-
-    return
+  downloadFile("/", "CurrentVersion.txt")
+  downloadFile("/", "PodOS.lua")
+  downloadFile("/lib/", "semver.lua")
+  downloadFile("/lib/", "youcubeapi.lua")
+  downloadFile("/lib/", "basalt.lua")
+  print("Pocket Pod Installed " .. currentVersion)
+  return;
     
 
 
